5a546462c3d8521e3aaa4048a3eae35c138adfe5
Guohong Liu <shokorapolar@yahoo.com>
2017-08-14 23:43:03 +0800

    add content md.

--- express.js
+++ express.js
@@ -5,18 +5,6 @@ var server = require('http').createServer(app);
 var router = express.Router();
 var marked = require('marked');
 
-marked.setOptions({
-    renderer: new marked.Renderer(),
-    gfm: true,
-    tables: true,
-    breaks: false,
-    pedantic: false,
-    sanitize: false,
-    smartLists: true,
-    smartypants: false
-});
-
-
 app.use('/', express.static(__dirname + '/vuewebpack'));
 
 router.get('/getnavbar', function (req, res) {
@@ -25,14 +13,21 @@ router.get('/getnavbar', function (req, res) {
 });
 
 router.get('/getmarkdown', function (req, res) {
-    fs.readFile('./www/gitbook.md', 'utf8', function (err, data) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.send(data);
-        console.log(data);
-    });
-    // res.send(md);
+    var b = ((fs.readFileSync(`./md/${req.query.name}.md`, 'utf8')).toString());
+    res.jsonp({a: b});
 });
 
 app.use(router);
 
 server.listen(8089);
+
+// marked.setOptions({
+//     renderer: new marked.Renderer(),
+//     gfm: true,
+//     tables: true,
+//     breaks: false,
+//     pedantic: false,
+//     sanitize: false,
+//     smartLists: true,
+//     smartypants: false
+// });
