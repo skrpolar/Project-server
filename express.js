1777e1cbcac63741a351a6f92aabef22fda347b1
Guohong Liu <shokorapolar@yahoo.com>
2017-08-21 19:12:02 +0800

    update search

--- express.js
+++ express.js
@@ -32,7 +32,7 @@ router.get('/getmarkdown', function (req, res) {
 
 router.get('/search', function (req, res) {
     var obj = getFileList('./md', req.query.lang);
-    coreContent(obj, req.query.s)
+    coreContent(obj, req.query.s, req.query.lang)
         .then(arr => {
             res.jsonp(arr);
         });
@@ -81,9 +81,10 @@ function readFile(path, filesList, lang) {
         if (states.isDirectory()) {
             readFile(`${path}/${file}`, filesList, lang);
         } else {
-            if (file.indexOf(`_${lang}`) !== -1) {
+            var index = file.indexOf(`_${lang}`);
+            if (index !== -1) {
                 var obj = {};
-                obj.name = file;
+                obj.name = file.substring(0, index);
                 obj.path = `${path}/${file}`;
                 filesList.push(obj);
             }
@@ -91,27 +92,29 @@ function readFile(path, filesList, lang) {
     }
 }
 
-function searchContent(obj, content) {
+function searchContent(obj, content, nav, lang) {
     return new Promise(function (resolve, reject) {
-        function ser(obj, content) {
+        function ser(obj, content, nav, lang) {
             for (var i in obj) {
                 (function (i) {
                     fs.readFile(obj[i].path, function (err, data) {
                         var re = /<[^>]+>/gi;
                         var str = marked(data.toString()).replace(re, '');
-                        var re2 = new RegExp(`.*${content}.*`,"gi");
-                        console.log();
-                        if(str.search(re2) !== -1) {
+                        var re2 = new RegExp(`.*${content}.*`, "gi");
+                        if (str.search(re2) !== -1) {
                             var index = str.search(re2);
-                            var n = str.substr(index, index + 100);
+                            var n = str.substr(index, 300);
+                            // var n = str;
                             n = n.replace(/[\n]/gi, '');
-                            c.push({
-                                name: obj[i].name,
-                                content: n
-                            });
+                            var name = iterator(nav, obj[i].name, lang);
+                            console.log(nav);
+                                c.push({
+                                    name: name,
+                                    content: n
+                                });
                         }
-                        num ++;
-                        if(num == obj.length) {
+                        num++;
+                        if (num == obj.length) {
                             resolve(c);
                         }
                     });
@@ -121,8 +124,33 @@ function searchContent(obj, content) {
 
         var num = 0;
         var c = [];
-        ser(obj, content);
+        ser(obj, content, nav, lang);
     });
+}
+
+function getNavInit() {
+    return new Promise(function (resolve, reject) {
+        fs.readFile('./www/navInit.json', 'utf8', function (err, data) {
+            resolve(data);
+        });
+    });
+}
+
+function iterator(obj, str, lang) {
+    for (var i in obj) {
+        console.log(i);
+        if (obj[i].hasOwnProperty('navActive')) {
+            
+            if (obj[i] == str) {
+                
+                return obj[i].text[lang];
+            } else {
+                this.iterator(obj[i].next, str, lang);
+            }
+        } else if (i == str) {
+            return obj[i].text[lang];
+        }
+    }
 }
 
 async function coreDir(path, fileName) {
@@ -134,9 +162,10 @@ async function coreDir(path, fileName) {
     }
 }
 
-async function coreContent(obj, content) {
+async function coreContent(obj, content, lang) {
     try {
-        const result = await searchContent(obj, content);
+        const nav = await getNavInit();
+        const result = await searchContent(obj, content, nav, lang);
         return result;
     } catch (error) {
         console.log(`core_dir_error: ${error}`);
